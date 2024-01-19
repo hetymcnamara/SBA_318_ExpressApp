770442567e1a2945c1cf8b959fd6330a36f5bbe0
hetymcnamara <150183328+hetymcnamara@users.noreply.github.com>
2024-01-19 16:50:38 -0500

    Update nature.js

--- data/nature.js
+++ data/nature.js
@@ -1,10 +1,10 @@
 const images = [
 
-    { id: 1, title: 'Peace by The Beach', url: '/nature/beach_top_view.jpg'},
-    { id: 2, title: 'Rocky Waters', url: '/nature/rocky_water.jpg'},
+    { id: 1, title: 'Calm on The Beach', url: '/nature/beach_top_view.jpg'},
+    { id: 2, title: 'Still Waters', url: '/nature/rocky_water.jpg'},
     { id: 3, title: 'Sandy Sahara', url: '/nature/sandy_sahara.jpg'},
-    { id: 4, title: 'Sharks', url: '/nature/dolphins.jpg'},
-    { id: 5, title: 'Snow Aurora', url: '/nature/snow_northern_lights.jpg'},
+    { id: 4, title: 'SDolphins', url: '/nature/dolphins.jpg'},
+    { id: 5, title: 'Snow Lights', url: '/nature/snow_northern_lights.jpg'},
     { id: 6, title: 'Snow Mountains', url: '/nature/snow_mountain.jpg'},
     { id: 7, title: 'Styled Garden', url: '/nature/styled_garden.jpg'},
     { id: 8, title: 'Sunset on the Beach', url: '/nature/sunset_beach_trees.jpg'},
